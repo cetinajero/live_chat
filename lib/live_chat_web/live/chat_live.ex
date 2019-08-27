8a7fbddc8b6375a42eb4f07e9d7b84a38caaef55
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2019-08-27 14:20:25 -0600

    2 commits from Chris
    Create local chat
    Add chat_server.ex
    
    https://github.com/alexgaribay/live_chat/commit/452876c0b3f09242900ea2faf79551b9ad96822f
    https://github.com/alexgaribay/live_chat/commit/a73eeedfcd279dbdc8fff983f9d74f484f488c74

--- lib/live_chat_web/live/chat_live.ex
+++ lib/live_chat_web/live/chat_live.ex
@@ -1,23 +1,55 @@
 defmodule LiveChatWeb.ChatLive do
   use Phoenix.LiveView
+  import Ecto.Changeset
+  alias LiveChatWeb.ChatView
+  alias LiveChat.PubSub
+  alias LiveChat.ChatServer, as: Chat
 
   def mount(%{user: user}, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(PubSub, "lobby")
+    end
     assigns = [
+      changeset: message_changeset(),
+      messages: Chat.get_messages(),
       user: user
     ]
     {:ok, assign(socket, assigns)}
   end
 
   def render(assigns) do
-    ~L"""
-    <div class="fullscreen">Welcome to chat, <%= @user.name%>!</div>
-    """
+    ChatView.render("chat.html", assigns)
   end
 
-  def handle_info(:count, socket) do
-    Process.send_after(self(), :count, 1_000)
-    count = socket.assigns.count + 1
+  def handle_info({:messages, messages}, socket) do
+    {:noreply, assign(socket, :messages, messages)}
+  end
 
-    {:noreply, assign(socket, :count, count)}
+  def handle_event("send", %{"chat" => attrs}, socket) do
+    attrs
+    |> message_changeset()
+    |> case do
+      %{valid?: true, changes: %{message: message}} ->
+        Chat.new_message(socket.assigns.user, message)
+        assigns = [
+          changeset: message_changeset()
+        ]
+        {:noreply, assign(socket, assigns)}
+        %{valid?: false} = changeset ->
+          {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  @types %{message: :string}
+
+  defp message_changeset(attrs \\ %{}) do
+    cast(
+      {%{}, @types},
+      attrs,
+      [:message]
+    )
+    |> validate_required([:message])
+    |> update_change(:message, &String.trim/1)
+    |> validate_required([:message])
   end
 end
