8ee1283e4f94d9e7299edb663e77edaf0bfa4ad0
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2019-08-27 09:56:12 -0600

    Add form validations

--- lib/live_chat_web/live/connect_live.ex
+++ lib/live_chat_web/live/connect_live.ex
@@ -1,9 +1,14 @@
 defmodule LiveChatWeb.ConnectLive do
   use Phoenix.LiveView
+  import Ecto.Changeset
   alias LiveChatWeb.ChatView
 
   def mount(_params, socket) do
-    {:ok, socket}
+    assigns = [
+      changeset: join_changeset()
+    ]
+
+    {:ok, assign(socket, assigns)}
   end
 
   def render(%{name: name} = assigns)do
@@ -19,14 +24,35 @@ defmodule LiveChatWeb.ConnectLive do
   end
 
   def handle_event("join", %{"user" => user}, socket) do
-    name = user["name"]
-    email = user["email"]
+    user
+    |> join_changeset()
+    |> Map.put(:action, :errors)
+    |> case do
+      %{valid?: true, changes: %{name: name, email: email}} ->
+        assigns = [
+          name: name,
+          email: email
+        ]
+        {:noreply, assign(socket, assigns)}
 
-    assigns = [
-      name: name,
-      email: email
-    ]
-
-    {:noreply, assign(socket, assigns)}
+      %{valid?: false} = changeset ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
   end
+
+  @types %{
+    name: :string,
+    email: :string
+  }
+
+  defp join_changeset(attrs \\ %{}) do
+    cast(
+      {%{}, @types},
+      attrs,
+      [:name, :email]
+    )
+    |> validate_required([:email, :name])
+    |> validate_format(:email, ~r/.+@.+/)
+  end
+
 end
